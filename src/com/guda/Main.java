7620ce6077bf4f7d31bf8132bfbdc7aaab3e92a6
egboyguda <e.gboyguda@gmail.com>
2020-03-11 16:58:56 +0800

    add function that read input from user and display function to display the array

--- src/com/guda/Main.java
+++ src/com/guda/Main.java
@@ -1,8 +1,30 @@
 package com.guda;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter the length of array");
+        int length = input.nextInt();
+        int [] array = readIntegers(length);
+        displayArr(array);
+
+    }
+    public static int[] readIntegers(int length){
+        Scanner input = new Scanner(System.in);
+        int [] array = new int[length];
+        for (int i = 0;i<length;i++){
+            System.out.println("please enter an element on the array:");
+            array[i]=input.nextInt();
+        }
+        return  array;
+    }
+    public static void displayArr(int[] array){
+        System.out.println(Arrays.toString(array));
     }
 }
